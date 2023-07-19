fb872c9490376055c0428165491450acc7743975
Leandro de Paula <leandro.paula@hndlabs.com.br>
2023-07-19 10:24:34 -0300

    Update Operadores Matematicos

--- operadores/operadores_matematicos.sql
+++ operadores/operadores_matematicos.sql
@@ -1,89 +1,88 @@
---Operador + 
-SELECT 1 + 3 
-
---Operador * 
-SELECT 3 * 2
+--Operador +  
+SELECT 1 + 3 
+  
+--Operador *  
+SELECT 3 * 2
 
---Operador - 
-SELECT 5 - 2
- 
---Operador / 
-SELECT 15 / 3
- 
-
---Operador % 
-SELECT 12%5
+--Operador -  
+SELECT 5 - 2
+   
+--Operador /  
+SELECT 15 / 3
+  
+--Operador %  
+SELECT 12 % 5
 
 --
 select ((1+4)*(3*3))/5
 
 /*
-Usando o operador de adição para calcular o número total de 
-horas de ausência do trabalho para cada funcionário.
+Usando o operador de adicao para calcular o numero total de 
+horas de ausencia do trabalho para cada funcionario.
 */
-use AdventureWorks2014
+USE AdventureWorks2014
 
-SELECT  p.firstname,             
-		p.lastname,             
-		e.vacationhours,             
-		e.sickleavehours,             
-		e.vacationhours + e.sickleavehours AS 'Horas Ausente' 
-FROM   humanresources.employee AS e        
-	JOIN person.person AS p          
-	ON e.businessentityid = p.businessentityid 
-	ORDER  BY 'Horas Ausente' ASC; 
-
-
+SELECT p.firstname,              
+	   p.lastname,              
+	   e.vacationhours,              
+	   e.sickleavehours,              
+	   e.vacationhours + e.sickleavehours AS 'Horas Ausente'  
+  FROM humanresources.employee AS e         
+	   JOIN person.person AS p           
+	     ON e.businessentityid = p.businessentityid  
+ ORDER BY 'Horas Ausente' ASC; 
 
 
---Usando subtração em uma instrução SELECT
-SELECT Max(taxrate) maximo,Min(taxrate) minimo,
-	   Max(taxrate) - Min(taxrate) AS 'Tax Rate Difference' 
-FROM   sales.salestaxrate 
-WHERE  stateprovinceid IS NOT NULL; 
+
+
+--Usando subtracao em uma instrucao SELECT
+SELECT Max(taxrate) maximo,Min(taxrate) minimo,
+	   Max(taxrate) - Min(taxrate) AS 'Tax Rate Difference'  
+  FROM sales.salestaxrate  
+ WHERE stateprovinceid IS NOT NULL; 
 
 /*
-O exemplo a seguir recupera o número de identificação do produto, 
-o nome, o preço de tabela e o novo preço de tabela de 
-todas as bicicletas mountain bike na tabela Product. 
-O novo preço de tabela é calculado usando o operador aritmético * 
-para multiplicar ListPrice por 1.15.
+O exemplo a seguir recupera o numero de identificao do produto, 
+o nome, o preco de tabela e o novo preco de tabela de 
+todas as bicicletas mountain bike na tabela Product. 
+O novo preco de tabela e calculado usando o operador aritmetico * 
+para multiplicar ListPrice por 1.15.
 */
 
-	SELECT  productid,        
-			NAME,        
-			listprice,        
-			listprice * 1.15 AS Novo_preco 
-			FROM   production.product 
-				WHERE  NAME LIKE 'Mountain-%' 
-	ORDER  BY productid ASC; 
+	SELECT productid,         
+		   [name],         
+		   listprice,         
+		   listprice * 1.15 AS Novo_preco  
+	  FROM production.product  
+	 WHERE [name] LIKE 'Mountain-%'  
+	 ORDER BY productid ASC; 
 
 
 /*
-O exemplo a seguir usa o operador aritmético de divisão para 
+O exemplo a seguir usa o operador aritmetico de divisao para 
 calcular a meta de vendas mensal da equipe de vendas em Ciclos de 12 meses
 */
 
-	SELECT  s.businessentityid AS SalesPersonID,        
-			firstname,        
-			lastname,        
-			salesquota,        
-			salesquota / 12    AS 'Meta Mensal' 
-			FROM   sales.salesperson AS s        
-				JOIN humanresources.employee AS e          
-				ON s.businessentityid = e.businessentityid        
-				JOIN person.person AS p          
-				ON e.businessentityid = p.businessentityid;
+	SELECT s.businessentityid AS SalesPersonID,         
+		   firstname,         
+		   lastname,         
+		   salesquota,         
+		   salesquota / 12 AS 'Meta Mensal'  
+	  FROM sales.salesperson AS s         
+		   JOIN humanresources.employee AS e           
+		     ON s.businessentityid = e.businessentityid         
+		   JOIN person.person AS p           
+		     ON e.businessentityid = p.businessentityid;
 
 
 /*
-O exemplo a seguir divide o número 38 por 5. 
+O exemplo a seguir divide o numero 38 por 5. 
 Isto resulta em 7 como a parte inteira do resultado 
-e demonstra como o módulo retorna o resto de 3.
+e demonstra como o modulo retorna o resto de 3.
 */
 
-	SELECT 38 / 5 AS Inteiro,        
-	       38 % 5 AS Restante
+	SELECT 38 / 5 AS Inteiro,         
+	       38 % 5 AS Restante
 
 
 		   
